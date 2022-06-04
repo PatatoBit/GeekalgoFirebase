fa8ec8d7f366498a104ae6ef76476c3bfabc41cb
PathonScript <pathonscript@gmail.com>
2022-06-04 08:31:36 +0700

    Simple Auth state change

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,18 @@
 <script>
 	import { auth, db } from '../firebase';
-	import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+	import { signInWithPopup, GoogleAuthProvider, onAuthStateChanged } from 'firebase/auth';
 	const provider = new GoogleAuthProvider();
+
+	let isLoggedin;
+
+	auth.onAuthStateChanged((user) => {
+		if(user){
+			isLoggedin = true;
+		} else {
+			isLoggedin = false;
+		}
+	})
+
 
 	function SignIn() {
 		signInWithPopup(auth, provider)
@@ -14,6 +25,12 @@
 	}
 </script>
 
-<div class="flex items-center justify-center w-full h-screen">
-	<button on:click={SignIn}>Sign in with Google</button>
-</div>
+{#if isLoggedin}
+	 <div class="flex items-center justify-center w-full h-screen">
+		 <h1 class="text-6xl">You have Logged in!</h1>
+	</div>
+{:else}
+	 <div class="flex items-center justify-center w-full h-screen">
+		 <button on:click={SignIn}>Sign in with Google</button>
+	 </div>
+{/if}
