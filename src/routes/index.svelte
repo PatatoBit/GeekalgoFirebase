7b4f6244dfb6893de7abf6b310affbd6bfad8dea
PathonScript <pathonscript@gmail.com>
2022-06-14 21:46:35 +0700

    setting document firestore

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,17 @@
 <script>
 	import { auth, db } from '../firebase';
+	import { doc, setDoc } from 'firebase/firestore';
 	import { signInWithPopup, GoogleAuthProvider, onAuthStateChanged } from 'firebase/auth';
 	const provider = new GoogleAuthProvider();
 
 	let isLoggedin;
+	let uid;
+	let inputValue;
 
 	auth.onAuthStateChanged((user) => {
 		if(user){
 			isLoggedin = true;
+			uid = user.uid;
 		} else {
 			isLoggedin = false;
 		}
@@ -23,11 +27,21 @@
 				console.log(err);
 			});
 	}
+
+	async function setDocument(){
+		const docRef = doc(db, "customers", uid)
+		await setDoc(docRef, {
+			name: inputValue
+		});
+	}
+
 </script>
 
 {#if isLoggedin}
-	 <div class="flex items-center justify-center w-full h-screen">
+	 <div class="flex flex-col items-center justify-center w-full h-screen">
 			<button on:click={() => auth.signOut()}>Sign Out</button>
+			<input class="flex border-2" type="text" bind:value={inputValue}>
+			<button on:click={setDocument}>Submit</button>
 	</div>
 {:else}
 	 <div class="flex items-center justify-center w-full h-screen">
